5d2bacffec990282fa1b860d157e8ffd05e07d0b
Niwatori401 <->
2024-08-29 17:44:28 -0700

    Add day-based difficulty Fix bloopy plush position on day end area

--- script/score_progress_bar.gd
+++ script/score_progress_bar.gd
@@ -1,24 +1,42 @@
 extends TextureProgressBar
 
+#ambient drop. 50 means a drop of 2/sec
 var seconds_to_fall : float = 50;
 
 var total_points_to_apply : float = 0;
-var max_points_per_application : float = 1;
+var target_loss_per_second : float = 100;
 
 var game_over := false;
 var game_win := false;
-
-const POINTS_FOR_KEY_HIT : float = 5;
-const POINTS_FOR_KEY_FAIL : float = -5;
 
 @export var face_graphics : Array[Texture2D];
 # if above this percent, show that index
 var face_graphic_threshold_percentage : Array[float] = [ 85, 60, 40, -100000 ];
 
+var point_gain_loss_by_day = {
+	1: [-8, 20],
+	2: [-13, 15],
+	3: [-13, 15],
+	4: [-18, 15],
+	5: [-18, 15],
+	6: [-23, 10],
+	7: [-28, 10],
+	8: [-28, 10],
+	9: [-28, 10],
+	10: [-28, 10],
+	11: [-28, 10],
+	12: [-28, 10],
+	13: [-28, 10],
+	14: [-28, 10],
+}
+
 func _on_game_win() -> void:
 	game_win = true;
 
+
+var day_number : int;
 func _ready() -> void:
+	day_number = floori(Inventory.get_save().get_value(Globals.SAVE_CATEGORY_PROGRESS, Globals.SAVE_KEY_DAY_NUMBER));
 	SignalBus.key_challenge_fail.connect(lose_points);
 	SignalBus.key_hit.connect(gain_points);
 	SignalBus.game_win.connect(_on_game_win);
@@ -58,7 +76,6 @@ func do_basic_fall_amount(delta : float) -> void:
 		value -= base_fall_amount;
 
 
-var target_loss_per_second : float = 20;
 func do_gained_change_amount(delta : float) -> void:
 	var amount_to_apply : float;
 	if abs(total_points_to_apply) >= delta * target_loss_per_second:
@@ -80,10 +97,15 @@ func do_gained_change_amount(delta : float) -> void:
 		return;
 	
 	value += amount_to_apply;
-	
+
+func get_gain_points_for_day():
+	return point_gain_loss_by_day[day_number][1];
+
+func get_loss_points_for_day():
+	return point_gain_loss_by_day[day_number][0];
 
 func lose_points() -> void:
-	total_points_to_apply += POINTS_FOR_KEY_FAIL;
+	total_points_to_apply += get_loss_points_for_day();
 
 func gain_points() -> void:
-	total_points_to_apply += POINTS_FOR_KEY_HIT;
+	total_points_to_apply += get_gain_points_for_day();
